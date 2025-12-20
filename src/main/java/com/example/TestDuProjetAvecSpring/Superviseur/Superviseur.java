9c0b51f24a344a8482cdf184be1c3e608bc87231
LegrosGabriel <133345446+LegrosGabriel@users.noreply.github.com>
2025-12-20 13:38:02 +0100

    Delete src directory

--- src/main/java/com/example/TestDuProjetAvecSpring/Superviseur/Superviseur.java
+++
@@ -1,6 +0,0 @@
-package com.example.TestDuProjetAvecSpring.Superviseur;
-
-public interface Superviseur {
-    void executer(String microservice);
-}
-
--- src/main/java/com/example/TestDuProjetAvecSpring/TypeMessage.java
+++
@@ -1,8 +0,0 @@
-package com.example.TestDuProjetAvecSpring;
-
-public enum TypeMessage {
-    APPEL_PATIENT,
-    DEMANDE_MEDICAL,
-    DEMANDE_AMBULANCE,
-    FIN_APPEL
-}
