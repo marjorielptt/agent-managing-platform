95140907707e33b3cc4ef54b36a62822dd19154b
LegrosGabriel <133345446+LegrosGabriel@users.noreply.github.com>
2025-12-26 12:09:15 +0100

    Update AmbulancierActor.java

--- service-hopital/src/main/java/com/hopital/AmbulancierActor.java
+++ service-hopital/src/main/java/com/hopital/AmbulancierActor.java
@@ -3,6 +3,20 @@ package com.hopital;
 import com.framework.Actor;
 import com.framework.ActorMessage;
 import com.framework.ActorSystem;
+import com.framework.GestionLog;
+
+public class AmbulancierActor extends Actor {
+
+    private final ActorSystem system;
+
+    public AmbulancierActor(String id, ActorSystem system) {
+        super(id);
+        this.system = system;package com.hopital;
+
+import com.framework.Actor;
+import com.framework.ActorMessage;
+import com.framework.ActorSystem;
+import com.framework.GestionLog;
 
 public class AmbulancierActor extends Actor {
 
@@ -18,8 +32,9 @@ public class AmbulancierActor extends Actor {
         if ("CRASH".equals(message.getPayload())) {
             throw new RuntimeException("• 🚨 Accident simulé ! L'ambulancier est hors service.");
         }
-
-        System.out.println("• 🚐 Ambulancier en route : " + message.getPayload());
+        String ordre = message.getPayload().toString();
+        System.out.println("• 🚐 Ambulancier en route : " + ordre);
+        GestionLog.ecrire("AMBULANCE", "Départ en intervention suite ordre : " + ordre);
 
         if (!"system".equals(message.getSenderId())) {
 
@@ -28,7 +43,32 @@ public class AmbulancierActor extends Actor {
             ActorMessage response = new ActorMessage(getId(), patientId, "service-patient", "🚑 Tenez bon, j'arrive !");
 
             system.send(response);
-            System.out.println("• ✅ Tentative de réponse envoyée au patient");
+        }
+    }
+
+    private String extractPatientId(String payload) {
+        return "patient-bob";
+    }
+}
+
+    }
+
+    @Override
+    public void onReceive(ActorMessage message) throws Exception {
+        if ("CRASH".equals(message.getPayload())) {
+            throw new RuntimeException("• 🚨 Accident simulé ! L'ambulancier est hors service.");
+        }
+        String ordre = message.getPayload().toString();
+        System.out.println("• 🚐 Ambulancier en route : " + ordre);
+        GestionLog.ecrire("AMBULANCE", "Départ en intervention suite ordre : " + ordre);
+
+        if (!"system".equals(message.getSenderId())) {
+
+            String patientId = extractPatientId(message.getPayload().toString());
+
+            ActorMessage response = new ActorMessage(getId(), patientId, "service-patient", "🚑 Tenez bon, j'arrive !");
+
+            system.send(response);
         }
     }
 
