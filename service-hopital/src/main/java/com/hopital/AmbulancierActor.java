2e1a97ac581a907fafe683634d8fbe650a738f9f
LegrosGabriel <133345446+LegrosGabriel@users.noreply.github.com>
2025-12-26 12:16:46 +0100

    Refactor AmbulancierActor by removing unused code

--- service-hopital/src/main/java/com/hopital/AmbulancierActor.java
+++ service-hopital/src/main/java/com/hopital/AmbulancierActor.java
@@ -11,46 +11,7 @@ public class AmbulancierActor extends Actor {
 
     public AmbulancierActor(String id, ActorSystem system) {
         super(id);
-        this.system = system;package com.hopital;
-
-import com.framework.Actor;
-import com.framework.ActorMessage;
-import com.framework.ActorSystem;
-import com.framework.GestionLog;
-
-public class AmbulancierActor extends Actor {
-
-    private final ActorSystem system;
-
-    public AmbulancierActor(String id, ActorSystem system) {
-        super(id);
         this.system = system;
-    }
-
-    @Override
-    public void onReceive(ActorMessage message) throws Exception {
-        if ("CRASH".equals(message.getPayload())) {
-            throw new RuntimeException("• 🚨 Accident simulé ! L'ambulancier est hors service.");
-        }
-        String ordre = message.getPayload().toString();
-        System.out.println("• 🚐 Ambulancier en route : " + ordre);
-        GestionLog.ecrire("AMBULANCE", "Départ en intervention suite ordre : " + ordre);
-
-        if (!"system".equals(message.getSenderId())) {
-
-            String patientId = extractPatientId(message.getPayload().toString());
-
-            ActorMessage response = new ActorMessage(getId(), patientId, "service-patient", "🚑 Tenez bon, j'arrive !");
-
-            system.send(response);
-        }
-    }
-
-    private String extractPatientId(String payload) {
-        return "patient-bob";
-    }
-}
-
     }
 
     @Override
